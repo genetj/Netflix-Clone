dcf36309174dd98712cf7f70516dc4086c3c437d
genetj <ashkie75@yahoo.com>
2025-02-17 21:05:49 -0800

    header and footer

--- Netflix-project/src/App.jsx
+++ Netflix-project/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+// import Home from './Pages/Home/Home'
+import Header from './Commponents/Header/Header'
+import Footer from './Commponents/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     
-      <div className='App'>
-          <h1>Hello</h1>
-      </div>
+      <>
+          {/* <Home/> */}
+          <Header/>
+          <Footer/>
+          
+      </>
 
     
   )
